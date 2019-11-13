3ff3166ff2286b2de17c45a3585b968949404c0e
quockhanh21299 <37074707+quockhanh21299@users.noreply.github.com>
2019-11-13 21:28:23 +0700

    upadte map
    update map info

---
+++ lib/src/model/trip_info_res.dart
@@ -0,0 +1,8 @@
+import 'package:fluter_ubertaxi/src/model/step_res.dart';
+
+class TripInfoRes {
+  final int distance; // met
+  final List<StepsRes> steps;
+
+  TripInfoRes(this.distance, this.steps);
+}
